f578a57b7658594d70c549c819551d7a8bf94875
swapnilnagaonkar <nagaonkarswapnil@gmail.com>
2020-08-16 18:51:16 +0530

    Code for the port

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs = require('hbs')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname,'../templates/views'))
 hbs.registerPartials(path.join(__dirname,'../templates/partials'))
@@ -14,6 +16,6 @@ app.get('', (request, response) => {
 })
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000')
 })
